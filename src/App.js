5a23a051e2de9348bfaf1a8463faa339bed51f1e
ricardosikic <ricardo.sikic@gmail.com>
2019-04-04 22:42:06 -0400

    finished

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+export class Relojito extends React.Component {
+
+ constructor(props) {
+   super(props);
+   this.state = { date: new Date() };
+ }
+
+ componentDidMount() {
+   this.timerID = setInterval( () => this.tick(), 1000 );
+ }
+
+
+ componentWillUnmount() {
+   clearInterval(this.timerID);
+ }
+
+ tick() {
+    this.setState({ 
+      date: new Date() 
+    });
+ }
+
+  render() {
+    return(
+     <div>
+      <h1>Hola Mundo!</h1>
+      <h2>Son las {this.state.date.toLocaleTimeString()}</h2>
+     </div> 
+    );
+  }
+}
+
